2e6ffc2998a24d1e9fb235f4cd8cc45333f50a95
cheol <cheolclass@gmail.com>
2026-02-19 20:49:31 +0900

    1

--- main3.cpp
+++ main3.cpp
@@ -55,22 +55,24 @@ int main()
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);  // 데이터를 현재 바인딩된 버퍼에 복사..  
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);  // 데이터를 현재 바인딩된 버퍼에 물리적 복사. CPU 정점 배열 데이터 -> GPU 버퍼 메모리로 통째로 복사
 	gladLoadGL();
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);  // VBO에 저장된 데이터 접근 밥법을 VAO에 알려줌(연결)
-	glEnableVertexAttribArray(0);
-	glBindVertexArray(0);  // 해제
-	glBindBuffer(GL_ARRAY_BUFFER, 0);  // 해제
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);  // VBO에 복사된 데이터를 어떻게 읽어야 하는지(접근 밥법)를 VAO에 알려줌(연결)
+	glEnableVertexAttribArray(0);  // 0번에 위치한 정점 속성 사용을 활성화. 비로소 GPU가 렌더링 시 해당 데이터를 참조 가능하게 됨
+	glBindVertexArray(0);  // VAO 해제. VBO보다 먼저 해제해야 함. VAO가 VBO의 상태를 기억하기 때문
+	glBindBuffer(GL_ARRAY_BUFFER, 0);  // VBO 해제
 
 	glUseProgram(shaderID);
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f); 
 
 	do
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 인덱스 버퍼 없이 정점 데이터를 순차적으로 읽어와 그림
+				// glDrawElements() -> 인덱스 버퍼를 통해 정점 데이터 참조 -> 중복X, 메모리 효율성 향상 
+				// 다음 예제에	서 다룸 
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
